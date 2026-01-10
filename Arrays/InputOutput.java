49c4e61acf2be0c335282d1334a8f10bd3e4818b
arindam <dasarindam342@gmail.com>
2026-01-10 22:33:15 +0530

    Add InputOutput class for array input and output functionality

---
+++ Arrays/InputOutput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class InputOutput {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array");
+    int size = sc.nextInt();
+    int arr[] = new int[size];
+
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+
+  }
+}
