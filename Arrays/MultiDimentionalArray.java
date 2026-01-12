4d70fbfe58a595ebccf620a4fb047c3100293e81
arindam <dasarindam342@gmail.com>
2026-01-12 20:29:45 +0530

    Fix array initialization and correct length typo in MultiDimentionalArray class

--- Arrays/MultiDimentionalArray.java
+++ Arrays/MultiDimentionalArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MultiDimentionalArray {
@@ -10,15 +11,35 @@ public class MultiDimentionalArray {
     //   {4,5},// 1st index
     //   {6,7,8,9}// 2nd index -> ar2D[2] = {6,7,8,9}
     // };
-    int [][] arr = new int[3][2];
+    int [][] arr = new int[3][3];
     System.out.println(arr.length);
     // Input 
     for(int row = 0; row <arr.length;row++){
       // for each col in every row 
-      for(int col = 0; col < arr[row].lenght; col ++){
-        arr[row][col] = sc.nextInt();
-        
+      for(int col = 0; col < arr[row].length; col ++){
+        arr[row][col] = sc.nextInt(); 
       }
+    } 
+    // ----------------------------------------------------------------
+    // // Output
+    //  for(int row = 0; row <arr.length;row++){
+    //   // for each col in every row 
+    //   for(int col = 0; col < arr[row].length; col ++){
+    //     System.out.print(arr[row][col] + " ");
+    //   }
+    //   System.out.println();
+    // }
+    // ------------------------------------------------------------------
+    // // Output using Arrays.toString method 
+    // for (int row = 0; row < arr.length; row++) {
+    //   System.out.println(Arrays.toString(arr[row]));
+    // }
+    // ------------------------------------------------------------------
+    // Output using Arrays.toString method and enhance for loop 
+    for(int [] a : arr){
+      System.out.println(Arrays.toString(a));
     }
+
+
   }
 }
