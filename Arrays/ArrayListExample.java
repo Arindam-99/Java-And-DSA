c4f75aadaceeb29eb603e09ecbeb583329b812d5
arindam <dasarindam342@gmail.com>
2026-01-18 19:27:13 +0530

    Add ArrayListExample class to demonstrate basic ArrayList functionality

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>(10);
+    list.add(265);
+    list.add(266);
+    list.add(267);
+    list.add(268);
+    list.add(269);
+    list.add(270);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    list.add(269);
+    System.out.println(list);
+
+  }
+}
