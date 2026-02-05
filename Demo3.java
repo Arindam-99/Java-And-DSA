95e71e11235c6549bdaf6a88650f08a18709a783
arindam <dasarindam342@gmail.com>
2026-02-05 20:16:14 +0530

    Update prompt for search value in Demo3 class

--- Demo3.java
+++ Demo3.java
@@ -13,7 +13,7 @@ public class Demo3 {
       numbers[i] = sc.nextInt();
 
     }
-    System.out.println("Enter the value you want to search for: ");
+    System.out.println("Enter the value you want to search for: X  ");
     int x = sc.nextInt();
     System.out.println("The output will be : ");
     for (int i = 0; i < numbers.length; i++) {
