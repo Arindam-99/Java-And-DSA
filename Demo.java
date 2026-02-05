54507912fe2febecb67f69ae1d07938cf5bfa8af
arindam <dasarindam342@gmail.com>
2026-02-05 19:58:27 +0530

    Add Demo and Demo2 classes for array input and search functionality

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+  public static void main(String[] args) {
+    int [] marks = new int[4];
+    marks[0] = 40;
+    marks[1] = 50;
+    marks[2] = 60;
+    marks[3] = 90;
+    // System.out.println(marks[0] +" " +marks[1] +" " + marks[2]+" " + marks[3]);
+    // using loop
+    for(int i = 0; i < 4 ; i++){
+      System.out.println(marks[i]);
+    }
+  }
+}
---
+++ Demo3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Demo3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array ");
+    int size = sc.nextInt();
+
+    int[] numbers = new int[size];
+
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("Enter the value: " + i);
+      numbers[i] = sc.nextInt();
+
+    }
+    System.out.println("Enter the value you want to search for: ");
+    int x = sc.nextInt();
+    System.out.println("The output will be : ");
+    for (int i = 0; i < numbers.length; i++) {
+      // System.out.println(numbers[i] + " ");
+      if (x == numbers[i]) {
+        System.out.println("X found at index " + i);
+      }
+    }
+    System.out.println();
+  }
+}
