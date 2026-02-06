efac5e663180adacf03cd86a1d499c9e7222c7ec
arindam <dasarindam342@gmail.com>
2026-02-06 19:03:29 +0530

    Fix print method in InsertionSort to use print instead of println for array output

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,7 +3,7 @@ package Sorting;
 public class InsertionSort {
   public static void printArray(int arr[]){
     for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i]+ " ");
+      System.out.print(arr[i]+ " ");
     }
   }
   public static void main(String[] args) {
