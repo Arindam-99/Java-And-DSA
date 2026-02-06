2635b76eb39e555cfc9e9e3758f261fec5518a42
arindam <dasarindam342@gmail.com>
2026-02-06 18:44:03 +0530

    Update comment for time complexity in SelectionSort class

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -10,6 +10,7 @@ public class SelectionSort {
   public static void main(String[] args) {
     int arr[] = { 7, 8, 3, 1, 2 };
     // selection sort 
+    // Time complexity = O(n^2)
     for(int i = 0; i<arr.length-1;i++){
       int smallest = i;
       for (int j = i+1; j < arr.length; j++) {
