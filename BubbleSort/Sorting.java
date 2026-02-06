321a6684f42d0e26674e235b3573222d1c9882df
arindam <dasarindam342@gmail.com>
2026-02-06 18:21:30 +0530

    Update comment for time complexity in Sorting class

--- BubbleSort/Sorting.java
+++ BubbleSort/Sorting.java
@@ -8,7 +8,7 @@ public class Sorting {
   }
   public static void main(String[] args) {
     int arr[] = { 7, 8, 3, 1, 2 };
-
+    // Time Complexity = O(n^2)
     // bubble sort
     for (int i = 0; i < arr.length - 1; i++) {
       for (int j = 0; j < arr.length - i - 1; j++) {
