0b109b606cceb1d8fefd6bf17aeb7d0493eca163
arindam <dasarindam342@gmail.com>
2026-02-07 20:33:03 +0530

    Add Recursion class implementing recursive and iterative factorial methods

---
+++ Recursion/Recursion.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class Recursion {
+    
+    // FIXED: Recursive approach
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1); 
+        }
+    }
+
+    // IMPROVED: Iterative approach
+    static int factorial_iterative_approach(int n) {
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("The value of factorial " + x + " is: " + factorial(x));
+        System.out.println("The value of factorial " + x + " is (iterative): " + factorial_iterative_approach(x));
+    }
+}
