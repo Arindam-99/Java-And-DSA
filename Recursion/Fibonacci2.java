e4852501e31500c02104363a5d9ddb2664534e6b
arindam <dasarindam342@gmail.com>
2026-02-08 19:32:07 +0530

    Add Fibonacci2 class implementing recursive Fibonacci calculation

---
+++ Recursion/Fibonacci2.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Fibonacci2 {
+  public static void main(String[] args) {
+    int n = 6;
+    for (int i = 0; i < n; i++) {
+        System.out.print(fibo(i) + " ");
+
+    }
+  }
+  static int fibo (int n){
+    if(n < 2){
+      return n;
+    }
+    return fibo(n-1) + fibo(n-2);
+  }
+}
