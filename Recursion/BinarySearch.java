f15069b065ce94063bbf60370146a1d619207204
arindam <dasarindam342@gmail.com>
2026-02-09 13:37:59 +0530

    Some changes in the code input

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -18,7 +18,7 @@ public class BinarySearch {
   }
 
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 55, 66 };
+    int[] arr = { 1, 2, 3, 4, 55, 66, 90, 87 };
     int target = 4;
     System.out.println(search(arr, target, 0, arr.length - 1));
   }
